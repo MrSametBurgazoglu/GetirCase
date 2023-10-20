cc2f5e8dd5dc0bb28e7d41a6ff0de92439cabeba
SametBurgazoglu <samet.burgazoglu@nextlua.com>
2023-10-20 12:08:54 +0300

    mongo connection string moved to render.com environment variables. typo fixed.

--- api/requests/MemoryRequests/keyValue.go
+++ api/requests/MemoryRequests/keyValue.go
@@ -2,5 +2,5 @@ package MemoryRequests
 
 type KeyValue struct {
 	Key   string `json:"key" example:"ben"`
-	Value string `json:"value" example:"sametR"`
+	Value string `json:"value" example:"samet"`
 }
